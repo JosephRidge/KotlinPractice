bb071272b76a5e737d4541e40a698bc6700ec300
josephridge <josephridge18@gmail.com>
2022-04-20 15:45:33 +0300

    SEt up of the Viewmodel Factory

--- Observables/app/src/main/java/com/jayr/observables/ui/numbers/NumbersViewModel.kt
+++ Observables/app/src/main/java/com/jayr/observables/ui/numbers/NumbersViewModel.kt
@@ -1,7 +1,16 @@
 package com.jayr.observables.ui.numbers
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class NumbersViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val _scoreReached = MutableLiveData<Int>()
+    val scoreReached: LiveData<Int>
+        get() = _scoreReached
+
+    init {
+        _scoreReached.value = 0
+    }
+
 }
---
+++ Observables/app/src/main/java/com/jayr/observables/ui/numbers/NumbersViewModelFactory.kt
@@ -0,0 +1,4 @@
+package com.jayr.observables.ui.numbers
+
+class NumbersViewModelFactory  {
+}
